172e248bec39cb5e19f23d5878eb020bb9add261
Olexander Zakatov <sashazakatov17@gmail.com>
2023-11-29 23:48:03 +0200

    update App component

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div>Привет</div>
-  );
-}
-
-export default App;
---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+
+const App = () => { 
+  return (
+    <div>Привет</div>
+  );
+}
+
+export default App;
