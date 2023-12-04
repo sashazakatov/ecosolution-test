a4df0251bf26e0d0e0b715177d5acd504ab4d922
Olexander Zakatov <sashazakatov17@gmail.com>
2023-12-04 15:58:04 +0200

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,15 +11,17 @@ const Header = () => {
                     <a href="/">
                         <img width={269} height={40} src={log} alt="logo" className={ css.header__logo } />
                     </a>
-                    <button type='button' className={ css.burger__menu }>
-                    <Icon width={16} height={16} id='menu' /> 
-                    </button>
-                    <a href="/" className={ css.header__touch }>
-                        <span className={css.touch__text}>Get in touch</span>
-                        <div className={css.touch__icon}>
-                            <Icon width={14} height={14} id="arrow-up" />
-                        </div>
-                    </a>
+                    <div className={ css.header__div }>
+                        <button type='button' className={ css.burger__menu }>
+                            <Icon width={16} height={16} id='menu' /> 
+                        </button>
+                        <a href="/" className={ css.header__touch }>
+                            <span className={css.touch__text}>Get in touch</span>
+                            <div className={css.touch__icon}>
+                                <Icon width={14} height={14} id="arrow-up" />
+                            </div>
+                        </a>
+                    </div>
                 </nav>
             </Container>
         </header>
