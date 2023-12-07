11b527b7cbc326345b69a405419e8061c6913bd9
Olexander Zakatov <sashazakatov17@gmail.com>
2023-12-07 22:55:00 +0200

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,14 +6,20 @@ const Hero = () => {
     return (
         <section className={ css.hero }>
             <Container>
-                <h1 className={css.hero__title}>Renewable energy for any task</h1>
-                <p className={css.hero__text}>Development and implementation of renewable non-polluting energy sources, generating power generation using energy wind, sun, water, biomass</p>
-                <a href="/" className={ css.header__learn_more }>
-                    <span className={ css.learn_more__text }>Learn more</span>
-                    <div className={css.learn_more__icon}>
-                        <Icon width={16} height={16} id="arrow-up" />
+                <div className={ css.hero__fff }>
+                    <h1 className={css.hero__title}>Renewable energy for any task</h1>
+                    <div className={ css.hero__div }>
+                        <p className={css.hero__text}>Development and implementation of renewable non-polluting energy sources, generating power generation using energy wind, sun, water, biomass</p>
+                        <div className={ css.header__learn_more }>
+                            <a href="/" className={ css.learn_more__link }>
+                                <span className={ css.learn_more__text }>Learn more</span>
+                                <div className={css.learn_more__icon}>
+                                    <Icon width={16} height={16} id="arrow-right" />
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </a>
+                </div>
             </Container>
         </section>
     );
